5ba496c6dee6c3fb8a91678c0871e67b80322168
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-17 21:39:45 +0900

    animate spinner

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"log"
-	"os/exec"
 	"os"
+	"os/exec"
+	"time"
 )
 
 func main() {
-	 file := os.Args[1]
-	 _, err := exec.Command("djvu2pdf", file).Output()
-	 if err != nil {
-		 log.Fatal(err)
-	 }
-	 fmt.Printf("\r%s is successfully converted to pdf", file)
+	file := os.Args[1]
+	go spinner(100 * time.Millisecond)
+	_, err := exec.Command("djvu2pdf", file).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("\033[F%s is successfully converted to pdf\n", file)
+}
+
+func spinner(delay time.Duration) {
+	fmt.Println()
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\033[F%c\n", r)
+			time.Sleep(delay)
+		}
+	}
 }
