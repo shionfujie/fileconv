5c88563e9a008789501fec685256744662cfe221
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-18 23:41:04 +0900

    encapsulate reset of cursor position

--- main.go
+++ main.go
@@ -10,20 +10,45 @@ import (
 
 func main() {
 	file := os.Args[1]
-	go spinner(100 * time.Millisecond)
+	spinner := NewSpinner(100 * time.Millisecond)
 	_, err := exec.Command("djvu2pdf", file).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("\033[Fconvert %s: DJVU file converted successfully to PDF file\n", file)
+	spinner.Stop()
+	fmt.Printf("convert %s: DJVU file converted successfully to PDF file\n", file)
 }
 
-func spinner(delay time.Duration) {
+type Spinner struct {
+	done chan struct{}
+}
+
+func NewSpinner(d time.Duration) *Spinner {
+	s := &Spinner{
+		done: make(chan struct{}, 1),
+	}
+	go startSpinner(s, d)
+	return s
+}
+
+func (s *Spinner) Stop() {
+	s.done <- struct{}{}
+}
+
+func startSpinner(s *Spinner, d time.Duration) {
+	ticker := time.NewTicker(d)
 	fmt.Println()
+loop:
 	for {
 		for _, r := range `-\|/` {
-			fmt.Printf("\033[F%c\n", r)
-			time.Sleep(delay)
+			select {
+			case <-ticker.C:
+				fmt.Printf("\033[F%c\n", r)
+			case <-s.done:
+				break loop
+			}
 		}
 	}
+	ticker.Stop()
+	fmt.Printf("\033[F")
 }
