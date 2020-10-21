d64cba47ef1debfb71694b7834a2732b0cd487dc
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-22 01:29:02 +0900

    use 'white-box' representation

--- spinner_test.go
+++ spinner_test.go
@@ -11,27 +11,27 @@ func TestSpinner(t *testing.T) {
 	count := 10
 	delta := 100 * time.Millisecond
 
-	spinAt := func(i int) string {
-		r := `-\|/`[i%4]
-		return fmt.Sprintf("%c\n", r)
-	}
-
 	var rb bytes.Buffer
 	spinner := NewFspinner(&rb, delta)
 	ticker := time.NewTicker(delta)
 
-	if rb.String() != "\n" {
+	expected := "\n"
+	if rb.String() != expected {
 		t.Errorf("got %q, expected immediate newline", rb.String())
 	}
 	for i := 0; i < count; i++ {
 		<-ticker.C
-		if rb.String() != spinAt(i) {
-			t.Errorf("got %q after %d ticks, expected %q", rb.String(), i, spinAt(i))
+		// Since the terminal is tty, we use ansi escaping to animate
+		// a spinner.
+		r := `-\|/`[i%4]
+		expected = fmt.Sprintf("%s\033[F%c\n", expected, r)
+		if rb.String() != expected {
+			t.Errorf("got %q after %d ticks, expected %q", rb.String(), i, expected)
 		}
 	}
 	spinner.Stop()
-	if rb.String() != "" {
-		t.Errorf("got %q, expected empty", rb.String())
+	if rb.String() != expected {
+		t.Errorf("got %q, expected to be unchanged", rb.String())
 	}
 	// Now test that the spinner stopped.
 	for i := 0; i < count; i++ {
