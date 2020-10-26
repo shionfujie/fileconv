108c7e70f4b76297be2512abbadc516368a4bd32
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-26 20:08:29 +0900

    segregate spinner

--- io/ioutil/spinner_test.go
+++ io/ioutil/spinner_test.go
@@ -1,10 +1,12 @@
-package main
+package ioutil_test
 
 import (
 	"bytes"
 	"fmt"
 	"testing"
 	"time"
+
+	. "github.com/shionfujie/fileconv/io/ioutil"
 )
 
 func TestSpinner(t *testing.T) {
@@ -19,13 +21,13 @@ func TestSpinner(t *testing.T) {
 	}
 	for i := 0; i < count; i++ {
 		// Let the spinner pass first.
-		time.Sleep(delta * 101/ 100)
+		time.Sleep(delta * 101 / 100)
 		// Since the terminal is tty, we use ansi escaping to animate
 		// a spinner.
 		r := `-\|/`[i%4]
 		expected = fmt.Sprintf("%s\033[F%c\n", expected, r)
 		if rb.String() != expected {
-			t.Errorf("after %d ticks: got %q, expected %q", i + 1, rb.String(), expected)
+			t.Errorf("after %d ticks: got %q, expected %q", i+1, rb.String(), expected)
 		}
 	}
 	spinner.Stop()
--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/shionfujie/fileconv/io/ioutil"
 )
 
 func main() {
 	fileNames := os.Args[1:]
 	outputs := make(chan string, len(fileNames))
 
-	spinner := NewSpinner(100 * time.Millisecond)
+	spinner := ioutil.NewSpinner(100 * time.Millisecond)
 	defer spinner.Stop()
 	for _, file := range fileNames {
 		go func(file string) {
@@ -95,54 +96,4 @@ func convert(file string) error {
 		return fmt.Errorf("failed to convert DJVU file to PDF file: %v", err)
 	}
 	return nil
-}
-
-type Spinner struct {
-	w    io.Writer
-	d    time.Duration
-	done chan struct{}
-}
-
-func NewSpinner(d time.Duration) *Spinner {
-	return NewFspinner(os.Stdout, d)
-}
-
-func NewFspinner(w io.Writer, d time.Duration) *Spinner {
-	s := &Spinner{
-		w:    w,
-		d:    d,
-		done: make(chan struct{}, 1),
-	}
-	startSpinner(s)
-	return s
-}
-
-func (s *Spinner) Stop() {
-	fmt.Fprintf(s.w, "\033[F")
-	s.done <- struct{}{}
-}
-
-func (s *Spinner) Printf(format string, a ...interface{}) {
-	// Stop the ticker temporarily
-	s.Stop()
-	fmt.Fprintf(s.w, format, a...)
-	// Restart a ticker
-	startSpinner(s)
-}
-
-func startSpinner(s *Spinner) {
-	t := time.NewTicker(s.d)
-	fmt.Fprintln(s.w)
-	go func() {
-	loop:
-		for i := 0; ; i = (i + 1) % 4 {
-			select {
-			case <-t.C:
-				fmt.Fprintf(s.w, "\033[F%c\n", `-\|/`[i])
-			case <-s.done:
-				t.Stop()
-				break loop
-			}
-		}
-	}()
 }
