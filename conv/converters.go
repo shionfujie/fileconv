ffb6a5a6baeac97f6270aead8f19daa9bc210d0f
shion.t.fujie <tonaluna32966@gmail.com>
2020-10-26 20:27:39 +0900

    segregate converter

---
+++ conv/converters.go
@@ -0,0 +1,69 @@
+package conv
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+type Converter struct {
+	SourceType string
+	TargetType string
+	file       string
+	cmd        *exec.Cmd
+	pathErr    *os.PathError
+}
+
+type cmd struct {
+	name       string
+	targetType string
+}
+
+var (
+	djvu2pdf = cmd{"djvu2pdf", "PDF"}
+)
+
+var cmds = map[string]cmd{
+	"DJVU": djvu2pdf,
+}
+
+func NewConverter(file string) (c *Converter) {
+	c = &Converter{
+		file: file,
+	}
+	c.SourceType = sourceType(file)
+	if _, err := os.Stat(file); err != nil {
+		c.pathErr = err.(*os.PathError)
+		return
+	}
+	cmd := cmds[c.SourceType]
+	c.TargetType = cmd.targetType
+	c.cmd = exec.Command(cmd.name, file)
+	return
+}
+
+func sourceType(file string) string {
+	return strings.ToUpper(filepath.Ext(file)[1:])
+}
+
+func (c *Converter) Convert() error {
+	if c.pathErr != nil {
+		return c.pathErr.Err
+	}
+	if _, err := c.cmd.Output(); err != nil {
+		return fmt.Errorf("failed to convert %s file to %s file: %v", c.SourceType, c.TargetType, err)
+	}
+	return nil
+}
+
+func convert(file string) error {
+	if _, err := os.Stat(file); err != nil {
+		return err.(*os.PathError).Err
+	}
+	if _, err := exec.Command("djvu2pdf", file).Output(); err != nil {
+		return fmt.Errorf("failed to convert DJVU file to PDF file: %v", err)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 	"time"
+
+	"github.com/shionfujie/fileconv/conv"
 
 	"github.com/shionfujie/fileconv/io/ioutil"
 )
@@ -19,7 +18,7 @@ func main() {
 	defer spinner.Stop()
 	for _, file := range fileNames {
 		go func(file string) {
-			c := NewConverter(file)
+			c := conv.NewConverter(file)
 			outputs <- formatMassage(file, c.SourceType, c.TargetType, c.Convert())
 		}(file)
 	}
@@ -36,64 +35,4 @@ func formatMassage(file string, sourceType string, targetType string, err error)
 		return fmt.Sprintf("Fail\tconvert %s: %v", file, err)
 	}
 	return fmt.Sprintf("ok  \tconvert %s: %s file converted successfully to %s file", file, sourceType, targetType)
-}
-
-type Converter struct {
-	SourceType string
-	TargetType string
-	file       string
-	cmd        *exec.Cmd
-	pathErr    *os.PathError
-}
-
-type cmd struct {
-	name       string
-	targetType string
-}
-
-var (
-	djvu2pdf = cmd{"djvu2pdf", "PDF"}
-)
-
-var cmds = map[string]cmd{
-	"DJVU": djvu2pdf,
-}
-
-func NewConverter(file string) (c *Converter) {
-	c = &Converter{
-		file: file,
-	}
-	c.SourceType = sourceType(file)
-	if _, err := os.Stat(file); err != nil {
-		c.pathErr = err.(*os.PathError)
-		return
-	}
-	cmd := cmds[c.SourceType]
-	c.TargetType = cmd.targetType
-	c.cmd = exec.Command(cmd.name, file)
-	return
-}
-
-func sourceType(file string) string {
-	return strings.ToUpper(filepath.Ext(file)[1:])
-}
-
-func (c *Converter) Convert() error {
-	if c.pathErr != nil {
-		return c.pathErr.Err
-	}
-	if _, err := c.cmd.Output(); err != nil {
-		return fmt.Errorf("failed to convert %s file to %s file: %v", c.SourceType, c.TargetType, err)
-	}
-	return nil
-}
-
-func convert(file string) error {
-	if _, err := os.Stat(file); err != nil {
-		return err.(*os.PathError).Err
-	}
-	if _, err := exec.Command("djvu2pdf", file).Output(); err != nil {
-		return fmt.Errorf("failed to convert DJVU file to PDF file: %v", err)
-	}
-	return nil
 }
